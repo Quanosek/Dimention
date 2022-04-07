48b48b5a5f745183b39223f101dcd0f895832980
Jakub Kłało <91485374+Quanosek@users.noreply.github.com>
2022-04-07 19:22:42 +0200

    Add files via upload

---
+++ events/guildCreate.js
@@ -0,0 +1,52 @@
+/* <--- Import ---> */
+
+const { MessageEmbed } = require('discord.js');
+const clr = require('colors');
+
+const config = require('../config.json');
+const realDate = require('../functions/realDate.js')
+
+
+/* <--- Event ---> */
+
+module.exports = {
+  name: 'guildCreate',
+
+  async execute(client, guild) {
+
+    /* <--- create log ---> */
+
+    console.log(`> ` + clr.brightCyan(`[${realDate()}]`) + ` Guild: ${guild.name}, ${guild.id}\n>> Bot ` + clr.brightGreen(`joined`) + ` to the server!`);
+
+    /* <--- welcome message ---> */
+
+    let channelToSend;
+
+    guild.channels.cache.forEach(channel => {
+      if (
+        channel.type === 'GUILD_TEXT' &&
+        channel.permissionsFor(guild.me).has(['SEND_MESSAGES', 'VIEW_CHANNEL'])
+      ) channelToSend = channel;
+    });
+
+    if (channelToSend) {
+
+      return channelToSend.send({
+        embeds: [new MessageEmbed()
+          .setColor(config.color1)
+          .setThumbnail(config.icon)
+          .setTitle('😄 | Cieszę się, że tu jestem!')
+          .setDescription(`
+opis
+        `)
+          .setFooter(`Bot stworzony przez: ${config.author}`)
+          .setTimestamp()
+        ]
+      }).catch(err => {
+        console.error(`> ` + clr.brightCyan(`[${realDate()}]`) + ` On guildCreate: ` + clr.Red(`Failed to create welcome-message (code ${err.code})`) + `.`);
+      });
+
+    };
+
+  }
+};
---
+++ events/ready.js
@@ -0,0 +1,23 @@
+/* <--- Import ---> */
+
+const ms = require('ms');
+const fetch = require('node-fetch');
+const clr = require('colors');
+
+const config = require('../config.json');
+const realDate = require('../functions/realDate.js')
+
+
+/* <--- Event ---> */
+
+module.exports = {
+  name: 'ready',
+
+  execute(client) {
+
+    /* <--- on-ready ---> */
+
+    console.log(`> ` + clr.brightCyan(`[${realDate()}]`) + ` Bot logged in successfully.\n`);
+
+  }
+};
